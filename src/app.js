72c786a1ccbe15a390193ebc35d2b8ea931f6cb0
Sreekanth Sundareswaran <ssundare@global.sial.com>
2018-04-10 13:51:50 +0530

    taking value from input field and logging to console on click of search

--- src/app.js
+++ src/app.js
@@ -3,16 +3,30 @@ import './app.css';
 import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 
 class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      query: ''
+    }
+  }
+
+  search(){
+    console.log('this.state', this.state);
+  }
+
   render(){
     return(
       <div className="App">
       <div className="App-title"> Music Master</div>
       <FormGroup>
         <InputGroup>
-          <FormControl type="text" placeholder="search for the artist"/>
-        <InputGroup.Addon>
-          <Glyphicon glyph="search"></Glyphicon>
-        </InputGroup.Addon>
+          <FormControl type="text" placeholder="search for the artist"
+            query={this.state.query}
+            onChange={event=>this.setState({query: event.target.value})}
+          />
+          <InputGroup.Addon onClick={()=>this.search()}>
+            <Glyphicon glyph="search"></Glyphicon>
+          </InputGroup.Addon>
         </InputGroup>
       </FormGroup>
       <div className="Profile">
