923c98b10736f5ac13a07141117e6fa31bd6bdda
Sreekanth Sundareswaran <ssundare@global.sial.com>
2018-04-13 17:20:35 +0530

    adding ternary operation and displaying images returned from the API call

--- src/profile.js
+++ src/profile.js
@@ -4,17 +4,23 @@ import './app.css';
 class Profile extends Component{
   render(){
     console.log('this.props from profile.js ...............',this.props);
-    let title = {volumeInfo:{title: '',publishedDate: '',subtitle: '', publisher:''}};
-    if(this.props.book1 !== null){
-      title = this.props.book1;
-      console.log("title....",title.volumeInfo.title);
-    }
+    let title = {volumeInfo:{title: '',publishedDate: '',subtitle: '', publisher:'', imageLinks:{smallThumbnail:''}}};
+    // if(this.props.book1 !== null){
+    //   title = this.props.book1;
+    // }
+    title = (this.props.book1 !== null) ? this.props.book1 : title;
+    console.log(title.volumeInfo.imageLinks.smallThumbnail);
     return(
       <div>
         <div>Book title: {title.volumeInfo.title}</div>
         <div>Published date: {title.volumeInfo.publishedDate}</div>
         <div>Sub title: {title.volumeInfo.subtitle}</div>
         <div>Publisher: {title.volumeInfo.publisher}</div>
+        <img
+          alt="Cover page"
+          className = "coverStyle"
+          src={title.volumeInfo.imageLinks.smallThumbnail}
+        />
       </div>
     )
   }
